9df1c5bb8d8f47ac42b5299b45269a93671ebff4
Armando Ramirez <armando.m.ramirez@gmail.com>
2022-08-24 13:48:33 -0700

    Get TicTacToe cross-compiling to mingwW64
    - Bump haskell.nix to a recent version
    - Use my fork with some small opengl x-compiling fixes (PR soon to come)
    - Bump GHC to 9.2.4
    - Add a dependency on freeglut to get that in scope
    - Add a freetype overlay to deal with its mingw issues
    - Add a freeglut overlay to properly x-compile it

--- default.nix
+++ default.nix
@@ -10,21 +10,52 @@ let
   # If haskellNix is not found run:
   #   niv add input-output-hk/haskell.nix -n haskellNix
 
+  freetype-overlay = self: super:
+    {
+      libpng = super.libpng.overrideAttrs(old: if !self.stdenv.hostPlatform.isWindows then {} else {
+        # freetype on windows complains about the presence of the .la file
+        # removing it seems to work
+        postInstall = ''
+          rm $out/lib/libpng16.la
+        '';
+      });
+      freetype = super.freetype.overrideAttrs(old: if !self.stdenv.hostPlatform.isWindows then {} else {
+        # See https://github.com/NixOS/nixpkgs/issues/160323
+        # We can just remove these and then we'll avoid the `wrapProgram` (and therefore bash) dependency
+        nativeBuildInputs = [];
+        postInstall = "";
+      });
+    };
+
+  freeglut-overlay = self: super:
+    {
+      freeglut = super.freeglut.overrideAttrs(old: if !self.stdenv.hostPlatform.isWindows then {} else {
+        buildInputs = [ ];
+        cmakeFlags = old.cmakeFlags ++ [
+          # See freeglut's README.mingw_cross
+          "-DCMAKE_TOOLCHAIN_FILE=mingw_cross_toolchain.cmake"
+          "-DGNU_HOST=x86_64-w64-mingw32"
+        ];
+      });
+    };
   # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
   pkgs = import
     # haskell.nix provides access to the nixpkgs pins which are used by our CI,
     # hence you will be more likely to get cache hits when using these.
     # But you can also just use your own, e.g. '<nixpkgs>'.
-    haskellNix.sources.nixpkgs-2009
+    haskellNix.sources.nixpkgs-unstable
     # These arguments passed to nixpkgs, include some patches and also
     # the haskell.nix functionality itself as an overlay.
-    haskellNix.nixpkgsArgs;
+    {
+      config = haskellNix.nixpkgsArgs.config;
+      overlays = haskellNix.nixpkgsArgs.overlays ++ [ freetype-overlay freeglut-overlay ];
+    };
 in pkgs.haskell-nix.project {
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "haskell-nix-project";
+    name = "TicTacToe";
     src = ./.;
   };
   # Specify the GHC version to use.
-  compiler-nix-name = "ghc8104"; # Not required for `stack.yaml` based projects.
+  compiler-nix-name = "ghc924";
 }
